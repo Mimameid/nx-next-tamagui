f8f7dbdc054f7245bd3ea04081c4427ed21eb2fc
Mimameid <ibrahimkeser@outlook.de>
2023-06-27 13:27:36 +0200

    Initial commit

---
+++ packages/next-app/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Stack, Text } from 'tamagui';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.none file.
+   */
+  return <Stack>{/* <Text>Hello</Text> */}</Stack>;
+}
---
+++ packages/next-app/next.config.js
@@ -0,0 +1,41 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withNx } = require('@nx/next');
+const { withTamagui } = require('@tamagui/next-plugin');
+
+process.env.TAMAGUI_TARGET = 'web';
+const disableExtraction = process.env.NODE_ENV === 'development';
+if (disableExtraction) {
+  console.log('Disabling static extraction in development mode for better HMR');
+}
+
+const tamaguiPlugin = withTamagui({
+  useReactNativeWebLite: true,
+  config: './tamagui.config.ts',
+  components: ['tamagui'],
+  importsWhitelist: ['constants.js', 'colors.js'],
+  logTimings: true,
+  disableExtraction,
+});
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = tamaguiPlugin({
+  ...nextConfig,
+  ...withNx({
+    ...nextConfig,
+    nx: {
+      // Set this to true if you would like to to use SVGR
+      // See: https://github.com/gregberge/svgr
+      svgr: false,
+    },
+  }),
+});
